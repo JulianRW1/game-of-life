9a199758818b9a31eae3fa87358036b70bbb7a13
unknown <julianrwalston@gmail.com>
2021-06-24 21:26:39 -0700

    added seed editor and settings

--- GameOfLife/src/com/julianrwalston/Main.java
+++ GameOfLife/src/com/julianrwalston/Main.java
@@ -3,7 +3,7 @@ package com.julianrwalston;
 public class Main {
 
     public static void main(String[] args) {
-        GameManager manager = new GameManager(30, 30);
+        GameManager manager = new GameManager(50, 50);
         manager.setUpGame();
     }
 }
